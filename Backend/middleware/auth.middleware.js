5dfa834cc4d05137ca9e107c2f962b36b0f6dffd
Rishi <kukadiyarishi895@gmail.com>
2025-12-19 15:25:08 +0530

    Auth middlware added

---
+++ Backend/middleware/auth.middleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import Student from "../models/student.model.js";
+
+export const verifyJWT = async (req, res, next) => {
+  try {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message : "Unauthorized access!!",
+        })
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await Student.findById(decoded.id).select("-password -refreshToken");
+    if (!user) {
+        return res.status(401).json({
+            success: false,
+            message : "Unauthorized access!!",
+        });
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({
+        success: false,
+        message : "Unauthorized access!!",
+    });
+  }
+};
